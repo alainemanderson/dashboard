77f6d8b473be0f14ed8a786ebb4d684dc54e67cd
Alaine Anderson <alainemanderson@gmail.com>
2018-08-01 19:24:09 -0600

    first commit

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,14 @@
+import '../styles/index.scss';
+
+import './masonry';
+import './charts';
+import './popover';
+import './scrollbar';
+import './search';
+import './sidebar';
+import './skycons';
+import './vectorMaps';
+import './datatable';
+import './datepicker';
+import './googleMaps';
+import './utils';
